90522f468c5c0eef6a7cd0b922982d5995ce36ad
Artem Morinov <trustinthelies@gmail.com>
2020-03-21 12:16:04 +0300

    preps completed, need to fix styles

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+
+module.exports = {
+  entry: { main: "./index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "index.[contenthash].css" }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: "./index.html",
+      filename: "index.html"
+    })
+  ]
+};
