2f68d72fe0e887fd860abe044ed8fb2761556381
Artem Morinov <trustinthelies@gmail.com>
2020-03-22 20:29:54 +0300

    styles are working, pics are saying fcku

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackMd5Hash = require("webpack-md5-hash");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: { main: "./index.js" },
@@ -17,17 +20,50 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        test: /\.css$/i,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico|woff2)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "./assets"
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          "file-loader?name=../images/[name].[ext]",
+          {
+            loader: "image-webpack-loader",
+            options: {}
+          }
+        ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "index.[contenthash].css" }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"]
+      },
+      canPrint: true
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       template: "./index.html",
-      filename: "index.html"
+      filename: "index.[hash].html"
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
